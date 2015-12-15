0f55880336b96648f718bca0a3c4aae4da5018da
RxBx <newbox15@New-Box-15s-Mac-Pro.local>
2015-12-14 18:24:30 -0800

    add foreach listing function with assoc ko array

--- js/app.js
+++ js/app.js
@@ -1,20 +1,25 @@
-var ViewModel = function () {
+var ViewModel = function() {
+   //var self = this;
 
-	this.clickCount = ko.observable(0);
-	this.name = ko.observable('Tabby');
-	this.imgSrc = ko.observable('img/22252709_010df3379e_z.jpg');
-	this.incrementCounter = function () {
-		this.clickCount(this.clickCount() +1);
-	};
-	this.catLevel = ko.computed(function () {
-		if (this.clickCount() < 11) {
-			return "new born";
-		} else if (this.clickCount() < 20) {
-			return "infant";
-		} else {
-			return "teen";
-		}
-	}, this);
+    this.clickCount = ko.observable(0);
+    this.name = ko.observable('Tabby');
+    this.imgSrc = ko.observable('img/22252709_010df3379e_z.jpg');
+    this.incrementCounter = function() {
+        this.clickCount(this.clickCount() + 1);
+    };
+
+    this.catLevel = ko.computed(function() {
+        if (this.clickCount() < 10) {
+            return "new born";
+        } else if (this.clickCount() < 20) {
+            return "infant";
+        } else {
+            return "teen";
+        }
+    }, this);
+
+    this.nickNames = ko.observableArray(['Tom','Dick', 'Spot']);
+
 };
 /*var ViewModel = function () {
 	this.catLevel = ko.computed(function () {
