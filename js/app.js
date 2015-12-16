489118db1e3e05b06a578bdef1038b534231e88f
RxBx <newbox15@New-Box-15s-Mac-Pro.local>
2015-12-16 15:10:36 -0800

    add list that uses click event data object to update current cat
    includes example use of using KO click event data object to inject into
    the currentCat. KO auto updates all views based on change in info/model.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,41 @@
-var Cat = function() {
-    this.clickCount = ko.observable(0);
-    this.name = ko.observable('Tabby');
-    this.imgSrc = ko.observable('img/22252709_010df3379e_z.jpg');
+var initialCats = [{
+    clickCount: 0,
+    name: 'Tabby',
+    imgSrc: 'img/434164568_fea0ad4013_z.jpg',
+    imgAttribution: 'https://www.flickr.com/photos/bigtallguy/434164568',
+    nickNames: ['Tom', 'Dick', 'Spot']
+}, {
+    clickCount: 0,
+    name: 'Tiger',
+    imgSrc: 'img/4154543904_6e2428c421_z.jpg',
+    imgAttribution: 'https://www.flickr.com/photos/xshamx/4154543904',
+    nickNames: ['Tomato ', 'Lettuce', 'Radish']
+}, {
+    clickCount: 0,
+    name: 'Scaredy',
+    imgSrc: 'img/22252709_010df3379e_z.jpg',
+    imgAttribution: 'https://www.flickr.com/photos/kpjas/22252709',
+    nickNames: ['Potato', 'Orange', 'Apple']
+}, {
+    clickCount: 0,
+    name: 'Shadow',
+    imgSrc: 'img/1413379559_412a540d29_z.jpg',
+    imgAttribution: 'https://www.flickr.com/photos/malfet/1413379559',
+    nickNames: ['Bean', 'Yam', 'Spinach']
+}, {
+    clickCount: 0,
+    name: 'Sleepy',
+    imgSrc: 'img/9648464288_2516b35537_z.jpg',
+    imgAttribution: 'https://www.flickr.com/photos/onesharp/9648464288',
+    nickNames: ['Onion', 'Lemon', 'Broccoli']
+}];
+
+
+var Cat = function(data) {
+    this.clickCount = ko.observable(data.clickCount);
+    this.name = ko.observable(data.name);
+    this.imgSrc = ko.observable(data.imgSrc);
+    this.nickNames = ko.observableArray(data.nickNames);
 
     this.catLevel = ko.computed(function() {
         if (this.clickCount() < 10) {
@@ -12,17 +46,29 @@ var Cat = function() {
             return "teen";
         }
     }, this);
-
-    this.nickNames = ko.observableArray(['Tom', 'Dick', 'Spot']);
-
 };
 
 
 var ViewModel = function() {
-    var self = this;
-    this.currentCat = ko.observable(new Cat() );
+    var self = this; //used to lock context for incrementCounter method defined below
+
+    this.catList = ko.observableArray([]);
+
+    initialCats.forEach(function(catItem) {
+        self.catList.push(new Cat(catItem));
+    });
+
+    this.currentCat = ko.observable(this.catList()[0]);
+
+    // receives as parameter the "current model value" aka KO obs array item from the click event
+    this.updateCurrentCat = function(clickedCat) {
+        //injects the "current model value" aka KO obs array item into the "currentCat"
+        self.currentCat(clickedCat);
+    };
+
     this.incrementCounter = function() {
         self.currentCat().clickCount(self.currentCat().clickCount() + 1);
+        //using self, now the action above will ignore any custom "with" context set in the HTML data bind
     };
 };
 
